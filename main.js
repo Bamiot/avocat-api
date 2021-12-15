355d82c4dd856d8340e97be0318cd77a312e95c5
louis.cordhomme <louis.cordhomme@gmail.com>
2021-12-15 18:12:21 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+const path = require('path')
+const http = require('http')
+const https = require('https')
+const Datastore = require('nedb')
+const cors = require('cors')
+const express = require('express')
+const session = require('express-session')
+const passport = require('passport')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(cookieParser())
+app.use(bodyParser.json())
+app.use(session({ secret: 'chatchienlapin', resave: false, saveUninitialized: false }))
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+http.createServer(app).listen(3000, () => {
+  console.log('Server listening on port 3000')
+})
