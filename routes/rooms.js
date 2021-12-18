e0ff40428583c03f29d3f0a598345213b703a1bf
louis.cordhomme <louis.cordhomme@gmail.com>
2021-12-18 17:24:10 +0100

    handle password at create and join room

--- routes/rooms.js
+++ routes/rooms.js
@@ -13,16 +13,18 @@ router.get('/join', (req, res) => {
   if (room_id !== '' && username !== '') {
     // check if the room a lready exist
     avocatRooms.findOne({ roomId: room_id }, (err, room) => {
-      if (err) res.status(400).send({ erreur: 'failed' })
-      else if (!room) res.status(400).send({ erreur: 'room does not exist !' })
+      if (err) res.status(400).send({ error: 'failed' })
+      else if (!room) res.status(400).send({ error: 'room does not exist !' })
+      else if (room.private && room.password !== req.query.password)
+        res.status(400).send({ error: 'wrong password' })
       // si la room n'existe pas on cherche si il existe un player du même pseudo qui est dans cette room
       else {
         avocatPlayers.findOne(
           { $and: [{ username: username }, { room: room_id }] },
           (err, player) => {
-            if (err) res.status(400).send({ erreur: 'failed' })
+            if (err) res.status(400).send({ error: 'failed' })
             else if (player)
-              res.status(400).send({ erreur: 'this pseudo already exist in this room' })
+              res.status(400).send({ error: 'this pseudo already exist in this room' })
             // et enfin si la room exist et qu'aucun joueur du même pseudo est dedans on créer le joueur
             else {
               let user = { username: username, isReady: false, room: room_id }
@@ -34,17 +36,20 @@ router.get('/join', (req, res) => {
         )
       }
     })
-  } else res.status(400).json({ erreur: 'roomId or username isnt valid' })
+  } else res.status(400).json({ error: 'roomId or username isnt valid' })
 })
 
 router.get('/create', (req, res) => {
-  const { room_id, username } = req.query
+  const { room_id, username, password } = req.query
   if (room_id !== '' && username !== '') {
     avocatRooms.findOne({ roomId: room_id }, (err, room) => {
-      if (err) res.status(400).send({ erreur: 'failed' })
-      else if (room) res.status(400).send({ erreur: 'room already exist !' })
+      if (err) res.status(400).send({ error: 'failed' })
+      else if (room) res.status(400).send({ error: 'room already exist !' })
       else {
-        let room = { roomId: room_id }
+        let room =
+          password && password.length > 2
+            ? { roomId: room_id, password }
+            : { roomId: room_id }
         avocatRooms.insert(room)
         let user = { username: username, isReady: false, room: room_id }
         avocatPlayers.insert(user)
@@ -52,7 +57,7 @@ router.get('/create', (req, res) => {
         res.status(201).send(room)
       }
     })
-  } else res.status(400).send({ erreur: 'roomId or username isnt valid' })
+  } else res.status(400).send({ error: 'roomId or username isnt valid' })
 })
 
 router.get('/publicRooms', (req, res) => {})
